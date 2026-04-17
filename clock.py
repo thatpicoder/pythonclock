4cdea10622a602d65d09dfb178162804d0f7210b
thatpicoder <dylananddaymee@icloud.com>
2026-04-16 20:13:53 -0400

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,11 @@
+import time
+
+while True:
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    if time.daylight:
+        tz = time.tzname[1]
+    else:
+        tz = time.tzname[0]
+    full_time = current_time + " " + tz
+    print('\rCurrent Time: ' + full_time, end='', flush=True)
+    time.sleep(1)
