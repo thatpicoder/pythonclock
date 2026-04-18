93858825bed0778ade24024df19c3064a18b4ac4
bitetheapple <dylananddaymee@icloud.com>
2026-04-18 11:35:19 -0700

    Rearrange print statements and maintain clock functionality

--- clock.py
+++ clock.py
@@ -1,5 +1,6 @@
 import time
-
+print("python clock, made by bitetheapple")
+print("ctrl+c to exit")
 while True:
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")
     if time.daylight:
@@ -7,6 +8,5 @@ while True:
     else:
         tz = time.tzname[0]
     full_time = current_time + " " + tz
-    print("python clock, made by bitetheapple")
     print('\rCurrent Time: ' + full_time, end='', flush=True)
     time.sleep(1)
